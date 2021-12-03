49ca12e232d69f6370dbae09a4a67d4e0f884f4d
HauntedHunteer <m.niemczuk@interia.pl>
2021-12-03 12:06:38 +0100

    docker stuff added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from "@angular/forms";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'fibonacci-numbers';
+
+  get f() {
+    return this.form.controls;
+  }
+
+  result?: any = undefined;
+
+  form = new FormGroup({
+    number: new FormControl('', Validators.required),
+  });
+
+  calculateFib() {
+    let inputNumber = Number(this.form.get('number')?.value);
+
+    if (Number.isInteger(inputNumber) && inputNumber >= 0) {
+      let a = 1;
+      let b = 1;
+      for (let i = 3; i <= inputNumber; i++) {
+        let c = a + b;
+        a = b;
+        b = c;
+      }
+      this.result = inputNumber === 0 ? "0" : b;
+
+    } else {
+      this.result = "Invalid input";
+    }
+  }
 }
